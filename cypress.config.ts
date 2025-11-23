dcfbcc48d6bd5eabf6e0744719cda7fe1a6f802f
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-11-23 20:53:24 +0200

    fix e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
+    // specPattern: "cypress/e2e/*.ts",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  types: ['cypress'],
 })
